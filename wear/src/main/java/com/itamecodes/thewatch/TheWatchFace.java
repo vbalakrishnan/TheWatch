0558b9d845ad8c53e3c4a6cb59c0b8b85578aa4b
Vivek <vivek@AN-VBALAKRISHNAN.local>
2015-04-15 17:47:17 -0500

    some work on the watch face

--- wear/src/main/java/com/itamecodes/thewatch/TheWatchFace.java
+++ wear/src/main/java/com/itamecodes/thewatch/TheWatchFace.java
@@ -1,5 +1,6 @@
 package com.itamecodes.thewatch;
 
+import android.animation.ObjectAnimator;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -12,6 +13,7 @@ import android.graphics.Matrix;
 import android.graphics.Paint;
 import android.graphics.Rect;
 import android.graphics.RectF;
+import android.graphics.Typeface;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
@@ -22,21 +24,27 @@ import android.support.wearable.watchface.WatchFaceStyle;
 import android.text.format.Time;
 import android.util.Log;
 import android.view.SurfaceHolder;
+import android.view.animation.LinearInterpolator;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.TimeZone;
 import java.util.concurrent.TimeUnit;
 
 /**
- * Created by vivek on 4/13/15.
+ * Created by vivek on 4/15/15.
  */
-public class TheWatchFaceService extends CanvasWatchFaceService {
-    @Override
+public class TheWatchFace extends CanvasWatchFaceService {
+    private static final long INTERACTIVE_UPDATE_RATE_MS = TimeUnit.SECONDS.toMillis(1);
+    private static final Typeface BOLD_TYPEFACE =
+            Typeface.create(Typeface.SANS_SERIF, Typeface.BOLD);
+    private static final Typeface NORMAL_TYPEFACE =
+            Typeface.create(Typeface.SANS_SERIF, Typeface.NORMAL);
+
+
     public Engine onCreateEngine() {
         return new Engine();
     }
-
-    private static final long INTERACTIVE_UPDATE_RATE_MS = TimeUnit.SECONDS.toMillis(1);
-
 
     private class Engine extends CanvasWatchFaceService.Engine {
         static final int MSG_UPDATE_TIME = 0;
@@ -78,91 +86,48 @@ public class TheWatchFaceService extends CanvasWatchFaceService {
         @Override
         public void onCreate(SurfaceHolder holder) {
             super.onCreate(holder);
-            Resources resources = TheWatchFaceService.this.getResources();
-            Drawable backgroundDrawable = resources.getDrawable(R.drawable.background, null);
-            Drawable secondDrawable = resources.getDrawable(R.drawable.secondhand, null);
-            mBackGroundBitmap = ((BitmapDrawable) backgroundDrawable).getBitmap();
-            mSecondHandBitmap = ((BitmapDrawable) secondDrawable).getBitmap();
-            setWatchFaceStyle(new WatchFaceStyle.Builder(TheWatchFaceService.this)
+            Resources resources = TheWatchFace.this.getResources();
+            setWatchFaceStyle(new WatchFaceStyle.Builder(TheWatchFace.this)
                     .setCardPeekMode(WatchFaceStyle.PEEK_MODE_SHORT)
                     .setBackgroundVisibility(WatchFaceStyle.BACKGROUND_VISIBILITY_INTERRUPTIVE)
-                    .setShowSystemUiTime(true)
+                    .setShowSystemUiTime(false)
                     .build());
+            Drawable backgroundDrawable = resources.getDrawable(R.drawable.thewatch_back, null);
+            mBackGroundBitmap = ((BitmapDrawable) backgroundDrawable).getBitmap();
             mTime = new Time();
-            paintBitmap = new Paint();
-            paintBitmap.setAntiAlias(true);
-            paintBitmap.setFilterBitmap(true);
-            mCenterPaint = new Paint();
-            mCenterPaint.setAntiAlias(true);
-            mCenterPaint.setFilterBitmap(true);
-            mCenterPaint.setStyle(Paint.Style.FILL);
-            mCenterPaint.setColor(Color.BLUE);
-            mCenterPaint.setStrokeWidth(30);
-
             mPaint = new Paint();
-            mPaint.setColor(Color.BLUE);
-            mPaint.setStyle(Paint.Style.FILL_AND_STROKE);
+            mPaint.setColor(Color.WHITE);
+            mPaint.setAntiAlias(true);
+            mPaint.setTextSize(30);
+            mPaint.setTypeface(BOLD_TYPEFACE);
+
         }
 
         @Override
         public void onDraw(Canvas canvas, Rect bounds) {
-
             mTime.setToNow();
             int width = bounds.width();
             int height = bounds.height();
             if (mBackGroundScaledBitmap == null || mBackGroundScaledBitmap.getWidth() != width || mBackGroundScaledBitmap.getHeight() != height) {
                 mBackGroundScaledBitmap = Bitmap.createScaledBitmap(mBackGroundBitmap, width, height, true);
-                mSecondHandScaledBitmap = Bitmap.createScaledBitmap(mSecondHandBitmap, mSecondHandBitmap.getWidth(), 120, true);
-                mHourScaledBitmap = Bitmap.createScaledBitmap(mSecondHandBitmap, mSecondHandBitmap.getWidth(), 80, true);
-                mMinuteScaledBitmap = Bitmap.createScaledBitmap(mSecondHandBitmap, mSecondHandBitmap.getWidth(), 100, true);
             }
-            canvas.drawBitmap(mBackGroundScaledBitmap, 0, 0, null);
-            float secRot = mTime.second;
-            if (secRot == 0.0) {
-                if (shouldRevCircle) {
-                    Log.v("vivekrot", "reverse true");
-                    shouldRevCircle = false;
-                } else {
-                    Log.v("vivekrot", "reverse false");
-                    shouldRevCircle = true;
-                }
-            }
-            //float secRot=10;
-            // if (!isInAmbientMode()) {
-            RectF oval = new RectF(0, 0, canvas.getWidth(), canvas.getHeight());
-            if (shouldRevCircle) {
-                Log.v("vivekrot", "sec=" + secRot + "---" + secRot * 6);
-                canvas.drawArc(oval, 270, secRot * 6, true, mPaint);
-            } else {
-                Log.v("vivekrot", "sec=" + secRot + "---" + secRot * 6);
-                float startdeg = (secRot * 6) - 90;
-                canvas.drawArc(oval, startdeg, (180 - startdeg) + 90, true, mPaint);
-            }
-               /* Matrix rotator = new Matrix();
-                rotator.postRotate(secRot * 6, 0, mSecondHandScaledBitmap.getHeight() - 30);
-                rotator.postTranslate(canvas.getWidth() / 2, ((canvas.getHeight() / 2) - mSecondHandScaledBitmap.getHeight()) + 30);
-                canvas.drawBitmap(mSecondHandScaledBitmap, rotator, paintBitmap);*/
-            //   }
+          //  if (!isInAmbientMode()) {
+                canvas.drawBitmap(mBackGroundScaledBitmap, 0, 0, null);
 
-            Matrix rotatorminute = new Matrix();
-            rotatorminute.postRotate((float) ((mTime.minute * 6) + (secRot * 0.1)), 0, mMinuteScaledBitmap.getHeight());
-            rotatorminute.postTranslate(canvas.getWidth() / 2, ((canvas.getHeight() / 2) - mMinuteScaledBitmap.getHeight()));
-            canvas.drawBitmap(mMinuteScaledBitmap, rotatorminute, paintBitmap);
+            Calendar calendar= Calendar.getInstance();
+            SimpleDateFormat dateFormat=new SimpleDateFormat("hh:mm:ss");
+            String timeNow=dateFormat.format(calendar.getTime());
+            float x=mPaint.measureText(timeNow);
+            float xOffset=(width-x)/2;
+            canvas.drawText(timeNow,xOffset,height/2,mPaint);
 
-            Matrix rotatorhour = new Matrix();
-            rotatorhour.postRotate((float) ((mTime.hour * 30) + (mTime.minute * 0.5) + (mTime.second * (30 / 3600))), 0, mHourScaledBitmap.getHeight());
-            rotatorhour.postTranslate(canvas.getWidth() / 2, ((canvas.getHeight() / 2) - mHourScaledBitmap.getHeight()));
-            canvas.drawBitmap(mHourScaledBitmap, rotatorhour, paintBitmap);
-            // canvas.drawCircle();
-
-            canvas.drawCircle(canvas.getWidth() / 2, canvas.getHeight() / 2, 5, mCenterPaint);
 
         }
 
         @Override
         public void onTimeTick() {
             super.onTimeTick();
-            // invalidate();
+            invalidate();
 
         }
 
@@ -177,9 +142,7 @@ public class TheWatchFaceService extends CanvasWatchFaceService {
         @Override
         public void onAmbientModeChanged(boolean inAmbientMode) {
             super.onAmbientModeChanged(inAmbientMode);
-
             invalidate();
-
             updateTimer();
         }
 
@@ -206,7 +169,7 @@ public class TheWatchFaceService extends CanvasWatchFaceService {
 
         private boolean shouldTimerBeRunning() {
 
-            //return isVisible() && !isInAmbientMode();
+          //  return isVisible() && !isInAmbientMode();
             return isVisible();
         }
 
@@ -216,7 +179,7 @@ public class TheWatchFaceService extends CanvasWatchFaceService {
             }
             mRegisteredTimeZoneReceiver = true;
             IntentFilter filter = new IntentFilter(Intent.ACTION_TIMEZONE_CHANGED);
-            TheWatchFaceService.this.registerReceiver(mTimeZoneReciever, filter);
+            TheWatchFace.this.registerReceiver(mTimeZoneReciever, filter);
         }
 
         private void unregisterReceiver() {
@@ -224,7 +187,8 @@ public class TheWatchFaceService extends CanvasWatchFaceService {
                 return;
             }
             mRegisteredTimeZoneReceiver = false;
-            TheWatchFaceService.this.unregisterReceiver(mTimeZoneReciever);
+            TheWatchFace.this.unregisterReceiver(mTimeZoneReciever);
         }
+
     }
 }
